e725adea0ea46027dae5e5b95409b4474cd82a02
Yuri Brasiliano <90057197+YuriBrasiliano@users.noreply.github.com>
2022-04-03 17:50:07 -0300

    Update Cliente.java

--- src/Cliente.java
+++ src/Cliente.java
@@ -10,5 +10,19 @@ public class Cliente {
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+	public String getDocumento() {
+		return nome;
+	}
+
+	public void setDocumento(String documento) {
+		this.documento = documento;
+	}
+	public String getNascimento() {
+		return nome;
+	}
+
+	public void setNascimento(String nascimento) {
+		this.nascimento = nascimento;
+	}
 
 }
